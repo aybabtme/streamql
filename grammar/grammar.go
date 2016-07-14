f2e81202b45f574c9a49cc2e80f58ea4869a1f22
Antoine Grondin <antoinegrondin@gmail.com>
2016-07-14 11:21:18 -0600

    syntactial & lexical grammars are LL(1)

--- grammar/grammar.go
+++ grammar/grammar.go
@@ -5,118 +5,140 @@ import "fmt"
 type token string
 
 const (
-	memberString = token("member")
-	closeArray   = token("]")
-	integer      = token("42")
-	rangeSep     = token(":")
-	epsilon      = token("")
-	openArray    = token("[")
-	thisSymbol   = token(".")
-	pipeSep      = token(" | ")
-	filterSep    = token(", ")
+	memberString = token(`member`)
+	closeArray   = token(`]`)
+	integer      = token(`42`)
+	rangeSep     = token(`:`)
+	epsilon      = token(``)
+	openArray    = token(`[`)
+	thisSymbol   = token(`.`)
+	pipeSep      = token(` | `)
+	filterSep    = token(`, `)
 )
 
-func parser(input <-chan token) error {
-	select {
-	case cur := <-input:
-		return start(cur, input)
-	default:
-		return nil
+type sentence struct {
+	Pop  func() (token, bool)
+	Push func(token)
+	Peek func() (token, bool)
+}
+
+func parser(input []token) error {
+	idx := 0
+	str := &sentence{
+		Pop: func() (token, bool) {
+			if len(input) >= idx {
+				return token(""), false
+			}
+			i := idx
+			idx++
+			return input[i], true
+		},
+		Push: func(t token) {
+			idx--
+			input[idx] = t
+		},
+		Peek: func() (token, bool) {
+			if len(input) >= idx {
+				return token(""), false
+			}
+			return input[idx], true
+		},
 	}
+	return start(str)
 }
 
 // Start -> Filters.
-func start(cur token, input <-chan token) error {
+func start(str *sentence) error {
 	return or(
 		"Start",
 		and("-> Filters", filters),
-	)(cur, input)
+	)(str)
 }
 
 // Filters -> Filter FilterChain.
-func filters(cur token, input <-chan token) error {
+func filters(str *sentence) error {
 	return or(
 		"Filters",
 		and("-> Filter FilterChain", filter, filterChain),
-	)(cur, input)
+	)(str)
 }
 
 // FilterChain -> filter_sep Filters
 //              | epsilon.
-func filterChain(cur token, input <-chan token) error {
+func filterChain(str *sentence) error {
 	return or(
 		"FilterChain",
 		and("-> filter_sep Filters", terminal(filterSep), filters),
 		and("-> epsilon", terminal(epsilon)),
-	)(cur, input)
+	)(str)
 }
 
 // Filter -> Selector SelectorChain.
-func filter(cur token, input <-chan token) error {
+func filter(str *sentence) error {
 	return or(
 		"Filter",
 		and("-> Selector SelectorChain", selector, selectorChain),
-	)(cur, input)
+	)(str)
 }
 
 // SelectorChain -> pipe_sep Filter
 //                | epsilon.
-func selectorChain(cur token, input <-chan token) error {
+func selectorChain(str *sentence) error {
 	return or(
 		"SelectorChain",
 		and("-> pipe_sep Filter", terminal(pipeSep), filter),
 		and("-> epsilon", terminal(epsilon)),
-	)(cur, input)
+	)(str)
 }
 
 // Selector -> this_symbol TypedSelector.
-func selector(cur token, input <-chan token) error {
+func selector(str *sentence) error {
 	return or(
 		"Selector",
 		and("-> this_symbol TypedSelector", terminal(thisSymbol), typedSelector),
-	)(cur, input)
+	)(str)
 }
 
 // TypedSelector -> member_string
 //                | open_array ArraySelector
 //                | epsilon.
-func typedSelector(cur token, input <-chan token) error {
+func typedSelector(str *sentence) error {
 	return or(
 		"TypedSelector",
 		and("-> member_string", terminal(memberString)),
 		and("-> open_array ArraySelector", terminal(openArray), arraySelector),
 		and("-> epsilon", terminal(epsilon)),
-	)(cur, input)
+	)(str)
 }
 
 // ArraySelector -> close_array
 //                | integer TypedArraySelector.
-func arraySelector(cur token, input <-chan token) error {
+func arraySelector(str *sentence) error {
 	return or(
 		"ArraySelector",
 		and("-> close_array", terminal(closeArray)),
 		and("-> integer TypedArraySelector", terminal(integer), typedArraySelector),
-	)(cur, input)
+	)(str)
 }
 
 // TypedArraySelector -> range_sep integer close_array
 //                     | close_array.
-func typedArraySelector(cur token, input <-chan token) error {
+func typedArraySelector(str *sentence) error {
 	return or(
 		"TypedArraySelector",
 		and("-> range_sep integer close_array", terminal(rangeSep), terminal(integer), terminal(closeArray)),
 		and("-> close_array", terminal(closeArray)),
-	)(cur, input)
+	)(str)
 }
 
 // scaffolding
 
-type rule func(token, <-chan token) error
+type rule func(*sentence) error
 
 func and(name string, rules ...rule) rule {
-	return func(cur token, input <-chan token) error {
+	return func(str *sentence) error {
 		for _, rule := range rules {
-			if err := rule(cur, input); err != nil {
+			if err := rule(str); err != nil {
 				return fmt.Errorf("%s\n-> %v", name, err)
 			}
 		}
@@ -125,19 +147,29 @@ func and(name string, rules ...rule) rule {
 }
 
 func or(name string, rules ...rule) rule {
-	return func(cur token, input <-chan token) error {
+	return func(str *sentence) error {
 		for _, rule := range rules {
-			if err := rule(cur, input); err != nil {
+			if err := rule(str); err != nil {
 				continue
 			}
+		}
+		cur, ok := str.Peek()
+		if !ok {
+			return fmt.Errorf("%s\nreached end of stream but production rules are not satisfied yet", name)
 		}
 		return fmt.Errorf("%s\nno rule can be matched for %#v", name, cur)
 	}
 }
 
 func terminal(t token) rule {
-	return func(cur token, input <-chan token) error {
-
+	return func(str *sentence) error {
+		cur, ok := str.Peek()
+		if !ok && t == epsilon {
+			return nil
+		}
+		if !ok {
+			return fmt.Errorf("expect token %#v, reached end of stream", t)
+		}
 		if cur != t {
 			return fmt.Errorf("expect token %#v, got %#v", t, cur)
 		}
--- grammar/grammar_test.go
+++ grammar/grammar_test.go
@@ -11,12 +11,7 @@ func TestGrammar(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Logf("parsing: %s", tt.sentence)
-		input := make(chan token, len(tt.tokens))
-		for _, t := range tt.tokens {
-			input <- t
-		}
-		close(input)
-		if err := parser(input); err != nil {
+		if err := parser(tt.tokens); err != nil {
 			t.Fatalf("can't parse input: %v", err)
 		}
 	}
