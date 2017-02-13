d1396feb7f579f4cdc56f6aba258beda783d695e
Antoine Grondin <antoinegrondin@gmail.com>
2017-02-12 21:52:06 -0400

    we be testing that vm

--- lang/spec/vm_test.go
+++ lang/spec/vm_test.go
@@ -1,15 +1,130 @@
 package spec
 
-import "testing"
-import "strings"
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/aybabtme/streamql/lang/spec/msg"
+	"github.com/aybabtme/streamql/lang/spec/msg/gomsg"
+)
 
 func TestVM(t *testing.T) {
-	r := strings.NewReader(`.`)
+	bd := gomsg.Build()
 
-	ast, err := Parse(r)
-	if err != nil {
-		t.Fatal(err)
+	tests := []struct {
+		name  string
+		query string
+		input []msg.Msg
+		want  []msg.Msg
+	}{
+		{"passthru of nothing",
+			"",
+			list(),
+			list(),
+		},
+		{"passthru of nothing",
+			".",
+			list(),
+			list(),
+		},
+		{"passthru",
+			"",
+			list(mustBool(bd, true)),
+			list(mustBool(bd, true)),
+		},
+		{"passthru",
+			".",
+			list(mustBool(bd, true)),
+			list(mustBool(bd, true)),
+		},
+		{"passthru if true",
+			"select(.)",
+			list(mustBool(bd, true), mustBool(bd, false)),
+			list(mustBool(bd, true)),
+		},
 	}
-	vm := &ASTInterpreter{tree: ast}
-	_ = vm
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ast, err := Parse(strings.NewReader(`.`))
+			if err != nil {
+				t.Fatal(err)
+			}
+			vm := &ASTInterpreter{tree: ast}
+
+			var got []msg.Msg
+			err = vm.Run(bd, ArraySource(tt.input), func(m msg.Msg) error {
+				if m == nil {
+					panic("derpo")
+				}
+				got = append(got, m)
+				return nil
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+			if want, got := tt.want, got; !reflect.DeepEqual(want, got) {
+				t.Errorf("want=%#v", want)
+				t.Errorf(" got=%#v", got)
+			}
+		})
+	}
+}
+
+func list(allOfThem ...msg.Msg) []msg.Msg { return allOfThem }
+
+type arraySource struct {
+	data []msg.Msg
+}
+
+func ArraySource(data []msg.Msg) msg.Source {
+	n := len(data)
+	i := 0
+	return func() (msg.Msg, bool, error) {
+		i++
+		if i > n {
+			return nil, false, nil
+		}
+		return data[i-1], i <= n, nil
+	}
+}
+
+func mustObject(bd msg.Builder, obj map[string]msg.Msg) msg.Msg {
+	return mustMsg(bd.Object(func(ob msg.ObjectBuilder) error {
+		for k, v := range obj {
+			err := ob.AddMember(k, func(_ msg.Builder) (msg.Msg, error) {
+				return v, nil
+			})
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}))
+}
+func mustArray(bd msg.Builder, arr []msg.Msg) msg.Msg {
+	return mustMsg(bd.Array(func(ab msg.ArrayBuilder) error {
+		for _, el := range arr {
+			err := ab.AddElem(func(_ msg.Builder) (msg.Msg, error) {
+				return el, nil
+			})
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}))
+}
+func mustString(bd msg.Builder, v string) msg.Msg { return mustMsg(bd.String(v)) }
+func mustInt(bd msg.Builder, v int64) msg.Msg     { return mustMsg(bd.Int(v)) }
+func mustFloat(bd msg.Builder, v float64) msg.Msg { return mustMsg(bd.Float(v)) }
+func mustBool(bd msg.Builder, v bool) msg.Msg     { return mustMsg(bd.Bool(v)) }
+func mustNull(bd msg.Builder) msg.Msg             { return mustMsg(bd.Null()) }
+
+func mustMsg(m msg.Msg, err error) msg.Msg {
+	if err != nil {
+		panic(err)
+	}
+	return m
 }
